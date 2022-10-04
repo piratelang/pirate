7c4ec76dbb0daad07d5f4bc86b1240b7a215680a
joerivanarkel <Joeri@vanarkel.net>
2022-10-04 09:34:55 +0200

    Test start On branch tests Changes to be committed: 	new file:   Common.Test/Common.Test.csproj 	new file:   Common.Test/LoggerTest.cs 	modified:   PirateLang.sln

---
+++ Common.Test/LoggerTest.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using Common.Enum;
+using Xunit;
+using System;
+
+namespace Common.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldLogToFIle()
+    {
+        //Arrange
+        bool result;
+        var logger = new Logger("0.1.2");
+        
+        //Act
+        try
+        {
+            using (FileStream fs = new FileStream($"{Logger.logName}.log", FileMode.Append, FileAccess.Write))
+            using (StreamWriter textWriter = new StreamWriter(fs))
+            {
+                Logger.Log("Test", "Test", LogType.INFO);
+            }
+            result = true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error:" + ex.Message);
+            result = false;
+        }
+
+        Assert.True(result);
+    }
+}
