362e95791c01eacab8ffb852e8c0eb4f078464cc
joerivanarkel <Joeri@vanarkel.net>
2022-10-06 23:09:55 +0200

    new file:   GRAMMAR.md 	new file:   NewParserTest/NewParserTest.csproj 	new file:   NewParserTest/Parser.cs 	new file:   NewParserTest/Program.cs 	modified:   PirateLang.sln

---
+++ NewParserTest/Parser.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewParserTest
+{
+    public class Parser
+    {
+        
+    }
+}
