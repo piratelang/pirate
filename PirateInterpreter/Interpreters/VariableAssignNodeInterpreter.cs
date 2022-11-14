37839fd324ae1743c5eedc5edf47a1fc7d1eb51e
joerivanarkel <Joeri@vanarkel.net>
2022-11-14 23:42:33 +0100

    Fixes and Improvements

--- PirateInterpreter/Interpreters/VariableAssignNodeInterpreter.cs
+++ PirateInterpreter/Interpreters/VariableAssignNodeInterpreter.cs
@@ -4,29 +4,29 @@ namespace PirateInterpreter.Interpreters;
 
 public class VariableAssignNodeInterpreter : BaseInterpreter
 {
-    public VariableAssignNode Node { get; set; }
+    public VariableAssignNode variableAssignNode { get; set; }
 
     public VariableAssignNodeInterpreter(INode node, InterpreterFactory InterpreterFactory, ILogger logger) : base(logger, InterpreterFactory)
     {
         if (node is not VariableAssignNode)throw new TypeConversionException(node.GetType(), typeof(VariableAssignNode));
-        Node = (VariableAssignNode)node;
+        variableAssignNode = (VariableAssignNode)node;
 
-        Logger.Log($"Created {this.GetType().Name} : \"{Node.ToString()}\"", this.GetType().Name, Common.Enum.LogType.INFO);
+        Logger.Log($"Created {this.GetType().Name} : \"{variableAssignNode.ToString()}\"", this.GetType().Name, Common.Enum.LogType.INFO);
     }
 
     public override List<BaseValue> VisitNode()
     {
-        if (Node.Identifier.Value.Value is not string)
+        if (variableAssignNode.Identifier.Value.Value is not string)
         {
-            if (Node.Identifier.Value.Value != null)
+            if (variableAssignNode.Identifier.Value.Value != null)
             {
-                throw new TypeConversionException(Node.Identifier.Value.Value.GetType(), typeof(string));
+                throw new TypeConversionException(variableAssignNode.Identifier.Value.Value.GetType(), typeof(string));
             }
             throw new TypeConversionException(typeof(string));
         }
 
-        var Identifier = (string)Node.Identifier.Value.Value;
-        SymbolTable.Instance(Logger).Set(Identifier, Node.Value);
+        var Identifier = (string)variableAssignNode.Identifier.Value.Value;
+        SymbolTable.Instance(Logger).Set(Identifier, variableAssignNode.Value);
 
         var variable = new Variable(Identifier, Logger, InterpreterFactory);
         return new List<BaseValue> { variable };
