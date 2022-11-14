2953957629eb1b071f1ace59de2a33bcb81c385f
joerivanarkel <Joeri@vanarkel.net>
2022-11-14 23:56:19 +0100

    Merge fixes

--- PirateInterpreter/Interpreters/VariableDeclarationInterpreter.cs
+++ PirateInterpreter/Interpreters/VariableDeclarationInterpreter.cs
@@ -4,29 +4,29 @@ namespace PirateInterpreter.Interpreters;
 
 public class VariableDeclarationInterpreter : BaseInterpreter
 {
-    public VariableDeclarationNode Node { get; set; }
+    public VariableDeclarationNode variableDeclarationNode { get; set; }
 
     public VariableDeclarationInterpreter(INode node, InterpreterFactory InterpreterFactory, ILogger logger) : base(logger, InterpreterFactory)
     {
         if (node is not VariableDeclarationNode)throw new TypeConversionException(node.GetType(), typeof(VariableDeclarationNode));
-        Node = (VariableDeclarationNode)node;
+        variableDeclarationNode = (VariableDeclarationNode)node;
 
-        Logger.Log($"Created {this.GetType().Name} : \"{variableAssignNode.ToString()}\"", this.GetType().Name, Common.Enum.LogType.INFO);
+        Logger.Log($"Created {this.GetType().Name} : \"{variableDeclarationNode.ToString()}\"", this.GetType().Name, Common.Enum.LogType.INFO);
     }
 
     public override List<BaseValue> VisitNode()
     {
-        if (variableAssignNode.Identifier.Value.Value is not string)
+        if (variableDeclarationNode.Identifier.Value.Value is not string)
         {
-            if (variableAssignNode.Identifier.Value.Value != null)
+            if (variableDeclarationNode.Identifier.Value.Value != null)
             {
-                throw new TypeConversionException(variableAssignNode.Identifier.Value.Value.GetType(), typeof(string));
+                throw new TypeConversionException(variableDeclarationNode.Identifier.Value.Value.GetType(), typeof(string));
             }
             throw new TypeConversionException(typeof(string));
         }
 
-        var Identifier = (string)variableAssignNode.Identifier.Value.Value;
-        SymbolTable.Instance(Logger).Set(Identifier, variableAssignNode.Value);
+        var Identifier = (string)variableDeclarationNode.Identifier.Value.Value;
+        SymbolTable.Instance(Logger).Set(Identifier, variableDeclarationNode.Value);
 
         var variable = new Variable(Identifier, Logger, InterpreterFactory);
         return new List<BaseValue> { variable };
