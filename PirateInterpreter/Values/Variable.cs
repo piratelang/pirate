84954d572070e3529a7aced1363a68381195330d
joerivanarkel <Joeri@vanarkel.net>
2022-11-16 11:39:19 +0100

    added check for log message

--- PirateInterpreter/Values/Variable.cs
+++ PirateInterpreter/Values/Variable.cs
@@ -12,32 +12,6 @@ public class Variable : BaseValue, IValue
         Value = SymbolTable.Instance(Logger).Get(value).Value;
     }
 
-    [Obsolete]
-    public BaseValue GetValue(string value)
-    {
-        // var resultNode = SymbolTable.Instance(Logger).Get(value);
-        // if (resultNode is not IValueNode)
-        // {
-
-        //     var interpreter = InterpreterFactory.GetInterpreter(resultNode, Logger);
-        //     var result = interpreter.VisitNode();
-        //     if (result.Count == 1)
-        //     {
-        //         return result[0].Value;
-        //     }
-        //     else
-        //     {
-        //         throw new Exception("Variable is not a single value");
-        //     }
-        // }
-        // else
-        // {
-        //     var valueNode = (IValueNode)resultNode;
-        //     return valueNode.Value.Value;
-        // }
-        return null;
-    }
-
     public override BaseValue OperatedBy(Token _operator, BaseValue other)
     {
         Logger.Log($"Variable {Value.ToString()}, {Value.GetType()} is being operated by {other.ToString()}, {other.GetType()} with {_operator.ToString()}", this.GetType().Name, LogType.INFO);
