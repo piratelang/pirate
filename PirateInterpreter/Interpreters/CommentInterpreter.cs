3cc062af7837e7d96cdb69635e4f9f188c160581
joerivanarkel <Joeri@vanarkel.net>
2022-11-29 14:50:28 +0100

    xml doc for commentinterpreter

--- PirateInterpreter/Interpreters/CommentInterpreter.cs
+++ PirateInterpreter/Interpreters/CommentInterpreter.cs
@@ -3,6 +3,9 @@ using PirateInterpreter.Values;
 
 namespace PirateInterpreter.Interpreters;
 
+/// <summary>
+/// An interpreter for comments which does nothing.
+/// </summary>
 public class CommentInterpreter : BaseInterpreter
 {
     public CommentInterpreter(INode node, InterpreterFactory interpreterFactory, ILogger logger) : base(logger, interpreterFactory)
