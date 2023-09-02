72ee98fdfd3dba299c883bd79de78c3e8fa136de
joerivanarkel <Joeri@vanarkel.net>
2023-09-03 00:34:51 +0200

    PIRATE-47 WIP

---
+++ Pirate.Lexer.F.Test/TokenRepositoryTest.fs
@@ -0,0 +1,31 @@
+﻿module Pirate.Lexer.F.Test.TokenRepositoryTest
+
+open Xunit
+open Pirate.Lexer.F
+open Pirate.Lexer.F.Tokens.Enums
+
+[<Fact>]
+let ShouldMakeNumber () =
+    let tokenRepository = new TokenRepository(new KeyWordService())
+    let text = "123"
+    let position = 0
+    
+    let result = tokenRepository.MakeNumber(text, position)
+    
+    Assert.Equal(3, result.Position)
+    Assert.Equal(VALUE, result.Token.TokenGroup);
+    Assert.Equal(INT, result.Token.TokenType);
+    Assert.Equal(123, result.Token.Value |> unbox<int>)
+    
+[<Fact>]
+let ShouldMakeFloat () =
+    let tokenRepository = new TokenRepository(new KeyWordService())
+    let text = "123.456"
+    let position = 0
+    
+    let result = tokenRepository.MakeNumber(text, position)
+    
+    Assert.Equal(7, result.Position)
+    Assert.Equal(VALUE, result.Token.TokenGroup)
+    Assert.Equal(FLOAT, result.Token.TokenType)
+    Assert.Equal(123.456, result.Token.Value |> unbox<float>)
---
+++ Pirate.Lexer.F.Test/TokenTest.fs
@@ -0,0 +1,15 @@
+﻿module Pirate.Lexer.F.Test.TokenTest
+
+open Xunit
+open Pirate.Lexer.F.Tokens
+open Pirate.Lexer.F.Tokens.Enums
+
+[<Fact>]
+let ShouldMatchToken () =
+    Token(VALUE, INT, 123).Matches(TokenType.INT, 123) |> Assert.True
+    
+[<Fact>]
+let ShouldNotMatchToken () =
+    Token(VALUE, INT, 123).Matches(TokenType.INT, 456) |> Assert.False
+
+
